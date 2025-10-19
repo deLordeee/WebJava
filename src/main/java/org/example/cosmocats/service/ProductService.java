64809b5dfa970d61b13a084b5434d3e3f6b6c2fa
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-10-19 23:51:29 +0300

    Rename methods in ProductService interface

--- src/main/java/org/example/cosmocats/service/ProductService.java
+++ src/main/java/org/example/cosmocats/service/ProductService.java
@@ -1,16 +1,18 @@
 package org.example.cosmocats.service;
 
-
-
-
 import org.example.cosmocats.dto.ProductDTO;
 
 import java.util.List;
 
 public interface ProductService {
-    List<ProductDTO> findAll();
-    ProductDTO findById(Long id);
-    ProductDTO create(ProductDTO productDTO);
-    ProductDTO update(Long id, ProductDTO productDTO);
-    void delete(Long id);
+
+    List<ProductDTO> listAllProducts();
+
+    ProductDTO getProductById(Long productId);
+
+    ProductDTO createProduct(ProductDTO productRequestDTO);
+
+    ProductDTO updateProduct(Long productId, ProductDTO productRequestDTO);
+
+    void deleteProduct(Long productId);
 }
