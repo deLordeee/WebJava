df73f836c6e463845a42dc1e5a791b8d20a103fa
Yevhenii Butkevych <143118683+deLordeee@users.noreply.github.com>
2025-12-01 13:57:37 +0200

    Update CosmocatsApplication.java

--- src/main/java/org/example/cosmocats/CosmocatsApplication.java
+++ src/main/java/org/example/cosmocats/CosmocatsApplication.java
@@ -2,7 +2,7 @@ package org.example.cosmocats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//main
+
 @SpringBootApplication
 public class CosmocatsApplication {
 
